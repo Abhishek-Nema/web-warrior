9d4c08c323e53fac8339c729da96d8fe2fc944f4
Abhishek Nema <92334663+Abhishek-Nema@users.noreply.github.com>
2024-12-03 17:54:44 +0000

    completed 6 challenges

---
+++ array_challenges.js
@@ -0,0 +1,35 @@
+// q1
+ let teaFlovors = ['green tea', "black tea", "oolong tea"]
+ let firstTea = teaFlovors[0]
+ console.log(firstTea);
+ 
+//  q2
+let cities = ["London", "Tokyo", "Paris", "New York"]
+let favouriteCity = cities[2]
+console.log(favouriteCity);
+
+// q3
+let teaTypes = ["herbal tea", "white tea", "masala chai"]
+teaTypes[1] = "jasmine tea"
+console.log(teaTypes);
+
+// q4
+let citiesVisited = ["Mumbai", "sydney"]
+citiesVisited.push("berlin")
+console.log(citiesVisited);
+
+// q5
+let teaOrders = ["chai", "iced tea", "matcha", "earl grey"]
+let lastOrder = teaOrders.pop()
+console.log(lastOrder);
+console.log(teaOrders);
+
+// q6
+let popularTeas = ["green tea", "oolong tea", "chai"]
+let softCopyTeas = popularTeas
+console.log(softCopyTeas);
+popularTeas.shift()
+console.log(softCopyTeas);
+
+
+
