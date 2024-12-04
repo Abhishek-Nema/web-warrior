7fdec7c0dd76c4489c27f5f780642db3f0d83a6d
Abhishek Nema <92334663+Abhishek-Nema@users.noreply.github.com>
2024-12-04 18:35:55 +0000

    completed till 6 ques of loops_challenges_2

--- loops_challenges_2.js
+++ loops_challenges_2.js
@@ -2,7 +2,7 @@
 let selectedTeas = []
 let teaArray = ["green tea", "black tea", "chai", "oolong tea"]
 for (let index = 0; index < teaArray.length; index++) {
-    if(teaArray[index]=="chai"){
+    if (teaArray[index] == "chai") {
         break
     }
     selectedTeas.push(teaArray[index])
@@ -12,8 +12,8 @@ console.log(selectedTeas);
 // q2
 let visitedCities = []
 let cityArray = ["London", "New York", "Paris", "Berlin"]
-for(let i=0; i < cityArray.length; i++){
-    if (cityArray[i]==="Paris"){
+for (let i = 0; i < cityArray.length; i++) {
+    if (cityArray[i] === "Paris") {
         continue
     }
     visitedCities.push(cityArray[i])
@@ -24,9 +24,49 @@ console.log(visitedCities);
 let numArr = [1, 2, 3, 4, 5]
 let smallNumbers = []
 for (const element of numArr) {
-    if (element==4){
+    if (element == 4) {
         break
     }
     smallNumbers.push(element)
 }
 console.log(smallNumbers);
+
+// q4
+let arrayOfTeas = ["chai", "green tea", "herbal tea", "black tea"]
+let preferredTeas = []
+
+for (const tea of arrayOfTeas) {
+    if (tea === "herbal tea") {
+        continue
+    }
+    preferredTeas.push(tea)
+}
+console.log(preferredTeas);
+
+// q5
+let cityPopulation = {}
+
+let citiesPopulation = {
+    "London": 890000,
+    "New York": 840000,
+    "Paris": 220000,
+    "Berlin": 350000
+}
+
+// q6
+
+let worldCities = {
+    "Sydney" : 5000000,
+    "Tokyo" : 9000000,
+    "Berlin" : 3500000,
+    "Paris" : 2200000
+}
+let largeCities = {}
+
+for (const city in worldCities) {
+    if(worldCities[city]<3000000){
+        continue
+    }
+    largeCities[city] = worldCities[city]
+}
+console.log(largeCities);
