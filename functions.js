2e5be5e6987a26df80778829fe5559ea0acc7b33
Abhishek Nema <92334663+Abhishek-Nema@users.noreply.github.com>
2024-12-05 19:15:56 +0000

    completed functions learning by questions

--- functions.js
+++ functions.js
@@ -18,5 +18,30 @@ let teaResult = orderTea("chai")
 console.log(teaResult);
 
 // q3
+let calculateTotal = (price, quantity) => price * quantity
+let totalCost = calculateTotal(2000, 5)
+console.log(totalCost);
+
+// q4
+function processTeaOrder(funcName){
+    return funcName("earl grey");
+}
+
+function makeTea(teaName){
+    return `Tea is ${teaName}`
+}
+
+let res = processTeaOrder(makeTea)
+console.log(res);
+
+// q5
+function createTeaMaker(){
+    return function(nameOfTea){
+        return `Making ${nameOfTea}`
+    }
+}
+let teaMaker = createTeaMaker()
+let result = teaMaker("green tea")
+console.log(result);
 
 
